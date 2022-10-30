e336a6d3d96a617a85f6d653c52a462512971bd1
George Hong <grh17@duke.edu>
2022-10-29 22:32:19 -0700

    Initial Commit

---
+++ Carica/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Carica
+//
+//  Created by George Hong on 10/29/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
